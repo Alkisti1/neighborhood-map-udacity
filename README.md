0de3c1b5ada2c9801f526ae25c29d9742adcc615
Bill Hinostroza <billh93@gmail.comgitconfig --global.name Bill>
2017-04-10 13:19:25 -0500

    Docs: Revamped README to include more detailed information relevant to this neighborhood project.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# neighborhood-map-udacity
+# Project 5: Neighborhood Map
+
+## What it is
+This Web Application showcases a Chicago neighborhood with 5 default markers provided with more information when clicked.
+
+## Features
+Filter locations: Includes a text input field or dropdown menu that filters the map markers and list items to locations
+matching the text input or selection. Filter function runs error-free.
+
+List View: A list-view of location names is provided which displays all locations by default, and displays the filtered
+subset of locations when a filter is applied. Clicking a location on the list displays unique information about the
+location, and animates its associated map marker by bouncing.
+
+
+## Required Libraries and Dependencies
+**** Everything is already included ****
+
+* Internet Connected Device
+* Modern Web Browser
+* Google Maps API
+* Yelp API
+* Knockout JS
+* jQuery
+* Bootstrap
+
+## Project contents
+This project consists the following files and directories:
+
+* `index.html` - The main html file that displays the map and is attached to all of the js scripts and stylesheets.
+* `styles.css` - CSS file that contains all of the styles for the web app.
+* `README.md` - This read me file.
+* `app.js` - Javascript file that contains the Google Maps API, Yelp API and Knockout JS code.
+
+
+## Credits:
+* Mike Fowler for Apple Maps CSS: https://snazzymaps.com/style/42/apple-maps-esque
+
---
+++ app.js
@@ -0,0 +1,86 @@
+/*******************************
+ Google Maps API
+ *******************************/
+// Creates a global map marker
+var map;
+// Create a new blank array for all the listing markers.
+var markers = [];
+// Create placemarkers array to use in multiple functions to have control
+// over the number of places that show.
+var placeMarkers = [];
+
+function initMap() {
+    // Create a styles array to use with the map.
+    var styles = [{
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [{"color": "#f7f1df"}]
+    }, {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [{"color": "#d0e3b4"}]
+    }, {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry",
+        "stylers": [{"visibility": "off"}]
+    }, {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [{"visibility": "off"}]
+    }, {
+        "featureType": "poi.business",
+        "elementType": "all",
+        "stylers": [{"visibility": "off"}]
+    }, {
+        "featureType": "poi.medical",
+        "elementType": "geometry",
+        "stylers": [{"color": "#fbd3da"}]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [{"color": "#bde6ab"}]
+    }, {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [{"visibility": "off"}]
+    }, {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [{"visibility": "off"}]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#ffe15f"}]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{"color": "#efd151"}]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#ffffff"}]
+    }, {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "black"}]
+    }, {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry.fill",
+        "stylers": [{"color": "#cfb2db"}]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{"color": "#a2daf2"}]
+    }];
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 41.970329, lng: -87.678778},
+        zoom: 13,
+        styles: styles,
+        mapTypeControl: false
+    });
+}
+
+/*******************************
+ Yelp API
+ *******************************/
